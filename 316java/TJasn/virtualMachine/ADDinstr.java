5692bc3cf0a716ff3b869ba696296fe8549a0c67
Steven Ou <osteve425@gmail.com>
2026-05-11 01:16:17 -0400

    adding in the arithmetic add instr

--- 316java/TJasn/virtualMachine/ADDinstr.java
+++ 316java/TJasn/virtualMachine/ADDinstr.java
@@ -6,14 +6,11 @@ import TJasn.TJ;
 
 public class ADDinstr extends ZeroOperandInstruction {
 
-  void execute () throws VirtualMachineHaltException
-  {
-      /* ???????? */
+  void execute() throws VirtualMachineHaltException {
+    EXPRSTACK[--ESP - 1] += EXPRSTACK[ESP];
   }
 
-  public ADDinstr ()
-  {
+  public ADDinstr() {
     super("ADD");
   }
 }
-
